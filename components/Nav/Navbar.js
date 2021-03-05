f3f923c9dd8078c44ceb4e8e945278b3d1bbc475
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-03-04 21:18:06 -0500

    styling the navbar

--- components/Nav/Navbar.js
+++ components/Nav/Navbar.js
@@ -2,7 +2,15 @@ import { MdLocalGroceryStore } from "react-icons/md";
 import Link from "next/link";
 import styled from "styled-components";
 
-const Logo = styled.h1``;
+const Logo = styled.div`
+  background-color: var(--black);
+  color: var(--white);
+  padding: 10px;
+  font-size: 22px;
+  font-weight: bold;
+  transform: skew(-7deg);
+  display: inline-block;
+`;
 function NavBar() {
   return (
     <div className="nav__bar">
